60ab39a28460efcfb69ebe122b197209116b33ac
Sudharsan S <sudharsanvsr@gmail.com>
2023-09-26 12:30:24 +0530

    Sample commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from "react";
 
 export default function App() {
-  return (
-    <div>App</div>
-  )
+
+    const name : string  = "Sudharsan";
+    const age : number = 21 ;
+    return (
+        <>
+        {name}
+        {age}
+        </>
+    )
+
 }
